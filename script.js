96a98ceaf0e70204380ed4daff42155e3ed820b3
Gilyazeev Adel <Atom735.2013@gmail.com>
2016-11-01 19:56:52 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-VK.init({apiId: 5702766});
 
-VK.Auth.login(alert("Logining"));
+
+var WindowVkLogin = window.open(
+	"https://oauth.vk.com/authorize?"
+	+"client_id=5702856"
+	+"&display=page"
+	+"&redirect_uri=https://atom735.github.io/-Maximum-/"
+	+"&scope=friends"
+	+"&response_type=token"
+	+"&v=5.59"
+	+"&state=123456", "_self");
